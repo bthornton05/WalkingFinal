47c1d615515db16773bee45408af05070446621f
bthornton05 <bthornton05@ladueschools.net>
2023-04-17 13:59:00 +0000

    modified:   WalkingSimulator/Monetization.java

--- WalkingSimulator/Monetization.java
+++ WalkingSimulator/Monetization.java
@@ -2,11 +2,24 @@ package WalkingSimulator;
 import java.util.Scanner;
 public class Monetization {
     private static int creditCardNumber;
-    private static int expDate;
+    private static String expDate;
     private static int ccv;
-    public Monetization(int num, int exp, int c){
-        creditCardNumber = num;
-        expDate = exp;
-        ccv = c;
+    private static String name;
+    public void Monetize(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your information");
+        System.out.print("Credit Card Number: ");
+        creditCardNumber = sc.nextInt();
+        System.out.println();
+        System.out.print("Expiration Date MM/YY");
+        expDate = sc.next();
+        System.out.println();
+        System.out.print("CCV: ");
+        ccv = sc.nextInt();
+        System.out.print("What is your mother's maiden name?: ");
+        name = sc.next();
+        System.out.println("Thank you, your information is:  \n Number: " + creditCardNumber + "\nExpiration Date: " + expDate + "\nCCV: " + ccv);
+        System.out.println("Your mother's maiden name is " + name);
+        sc.close();
     }
 }
